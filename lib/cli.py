8043edde94b923539039543461a8aec86de7c1ce
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-28 13:52:52 +0300

    added an if statement to main function

--- lib/cli.py
+++ lib/cli.py
@@ -8,5 +8,24 @@ def main():
     menu()
     choice = input("> ").strip()
 
+    if choice == "0":
+      exit_program()
+    elif choice == "1":
+      list_libraries()
+    elif choice == "2":
+      create_library()
+    elif choice == "3":
+      delete_library()
+    elif choice == "4":
+      view_library_books()
+    elif choice == "5":
+      list_books()
+    elif choice == "6":
+      create_book()
+    elif choice == "7":
+      delete_book()
+    else:
+      print("Invalid choice. Please try again.")
+
 def menu():
   pass
