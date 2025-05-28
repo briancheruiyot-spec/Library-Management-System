3e357bf209f43ce018b9b278aa00ec80953d18e4
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-28 12:17:35 +0300

    added Library class

--- lib/db/models.py
+++ lib/db/models.py
@@ -5,3 +5,12 @@ Base = declarative_base()
 engine = create_engine('sqlite:///lib/db/library.db')
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+class Library(Base):
+  __tablename__ = 'libraries'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String, nullable=False)
+  books = relationship('Book', back_populates='library', cascade='all, delete')
+
