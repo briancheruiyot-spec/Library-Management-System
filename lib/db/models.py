c7fc0e44c427c37fbd25e5dbd080dbf5947e85b3
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-28 12:02:43 +0300

    added variables Base Engine Session and session

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,2 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, declarative_base, sessionmaker
+
+Base = declarative_base()
+engine = create_engine('sqlite:///lib/db/library.db')
+Session = sessionmaker(bind=engine)
+session = Session()
