b91a045e5025e3f5bf47c0ae119efae1cdf67764
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-28 12:47:19 +0300

    added seed method

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,3 +1,9 @@
 from faker import Faker
 from models import Base, engine, session, Library, Book
 import random
+
+fake = Faker()
+
+def seed():
+  Base.metadata.drop_all(engine)
+  Base.metadata.create_all(engine)
